789a16fe25d555f8314b02231244d71a99b3f57d
Sugiuro <sugiuro.dev@gmail.com>
2019-12-23 18:02:12 +0100

    Added tests for Fate Simulation computation

--- tests/test_research_ships.py
+++ tests/test_research_ships.py
@@ -46,3 +46,20 @@ class TestComputeRequiredBlueprintsForStrengthenLevel(unittest.TestCase):
                 target_level=30, current_level=20, current_bp=90,
                 is_decisive_ship=False),
             120)
+
+    def test_computeRequiredBlueprintsForFateSimulation(self):
+        self.assertEqual(
+            ResearchWidget.computeRequiredBlueprintsForFateSimulation(
+                target_phase=5, current_phase=4, current_bp=0), 65)
+        self.assertEqual(
+            ResearchWidget.computeRequiredBlueprintsForFateSimulation(
+                target_phase=4, current_phase=5, current_bp=0), 0)
+        self.assertEqual(
+            ResearchWidget.computeRequiredBlueprintsForFateSimulation(
+                target_phase=3, current_phase=4, current_bp=0), 0)
+        self.assertEqual(
+            ResearchWidget.computeRequiredBlueprintsForFateSimulation(
+                target_phase=5, current_phase=4, current_bp=10), 55)
+        self.assertEqual(
+            ResearchWidget.computeRequiredBlueprintsForFateSimulation(
+                target_phase=5, current_phase=3, current_bp=50), 55)
