97375c8537264efda63e021505de57704a8cf775
Sugiuro <sugiuro.dev@gmail.com>
2019-12-23 15:37:08 +0100

    Display Fate Simulation table for PR S1 ships

--- azuriris/ui/research_widget.py
+++ azuriris/ui/research_widget.py
@@ -1,7 +1,13 @@
-from PySide2.QtWidgets import QWidget
-from PySide2.QtGui import QPixmap, QImage
+from PySide2.QtWidgets import (QWidget, QLabel, QTableWidget, QSizePolicy,
+                               QTableWidgetItem)
+from PySide2.QtGui import QPixmap, QImage, QFont
+from PySide2.QtCore import QSize
 
 from .ui.research_widget import Ui_ResearchWidget
+
+# Indicate the maximum Research Season where a Fate Simulation is possible
+# Currently, only PR S1 have Fate Simulation
+MAX_FATE_SIMUL_SEASON = 1
 
 
 class ResearchWidget(QWidget, Ui_ResearchWidget):
@@ -15,3 +21,39 @@ class ResearchWidget(QWidget, Ui_ResearchWidget):
         qimg = QImage.fromData(self.researchShip.Shipfu.image)
         self.shipIconLabel.setPixmap(QPixmap.fromImage(qimg))
         self.shipNameLabel.setText(self.researchShip.Shipfu.name)
+
+        if self.researchShip.ResearchShip.season <= MAX_FATE_SIMUL_SEASON:
+            self.addFateSimulationTable()
+
+    def addFateSimulationTable(self):
+        boldFont = QFont()
+        boldFont.setBold(True)
+
+        self.fateSimulationTitleLabel = QLabel(
+            "Blueprints needed per Fate Simulation level")
+        self.fateSimulationTitleLabel.setFont(boldFont)
+        self.gridLayout.addWidget(self.fateSimulationTitleLabel, 4, 0, 1, 6)
+
+        # Setup table widget
+        self.fateSimulationTableWidget = QTableWidget(self)
+        sizePolicy = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        self.fateSimulationTableWidget.setSizePolicy(sizePolicy)
+        self.fateSimulationTableWidget.setMinimumSize(QSize(620, 55))
+        self.fateSimulationTableWidget.setRowCount(1)
+        self.fateSimulationTableWidget.setColumnCount(5)
+
+        self.fateSimulationTableWidget.horizontalHeader().setVisible(True)
+        self.fateSimulationTableWidget.verticalHeader().setVisible(True)
+
+        item = QTableWidgetItem()
+        self.fateSimulationTableWidget.setVerticalHeaderItem(0, item)
+        self.fateSimulationTableWidget.verticalHeaderItem(0).setText(
+            "Required blueprints")
+
+        for i in range(5):
+            item = QTableWidgetItem()
+            self.fateSimulationTableWidget.setHorizontalHeaderItem(i, item)
+            self.fateSimulationTableWidget.horizontalHeaderItem(i).setText(
+                f"Phase {i + 1}")
+
+        self.gridLayout.addWidget(self.fateSimulationTableWidget, 5, 0, 1, 6)
